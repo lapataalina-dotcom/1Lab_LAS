58bc138c6067fa9a661946dc38fcf0a24797be04
LAS <lapataalina@gmail.com>
2026-09-12 19:29:50 +0300

    main я снова изменила  Main.java (для конфликта слияния)

--- Main.java
+++ Main.java
@@ -1,1 +1,1 @@
-public class Main { public static void main(String[] args) { System.out.println("Hello, Git, --v 2 (пусть будет так)"); } }
+public class Main { public static void main(String[] args) { System.out.println("Hello from MAIN branch"); } }
