f105c7d46ead5c3f8f162b9afc5a3526bf168c6e
LAS <lapataalina@gmail.com>
2026-09-12 19:01:13 +0300

    Dev ветка, изменение 1 в Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,1 @@
-public class Main { public static void main(String[] args) { System.out.println("Hello from MAIN branch"); } }
+public class Main { public static void main(String[] args) { System.out.println("Hello from MAIN and Dev branches (merged)"); } }
